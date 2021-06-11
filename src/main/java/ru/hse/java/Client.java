f121bbb46460df4e3619e730f38d4508f39159f6
German Tarabonda <tarabonda@bk.ru>
2021-06-11 21:12:29 +0300

    Add more parameters for client

--- src/main/java/ru/hse/java/Client.java
+++ src/main/java/ru/hse/java/Client.java
@@ -16,10 +16,14 @@ public class Client implements Callable<Void> {
 
     private final int id;
     private final int n; // quantity of arrays' elements
+    private final int x;
+    private final int delta;
 
-    public Client(int id, int n) {
+    public Client(int id, int n, int x, int delta) {
         this.id = id;
         this.n = n;
+        this.x = x;
+        this.delta = delta;
     }
 
     private Numbers generateData() {
@@ -36,33 +40,37 @@ public class Client implements Callable<Void> {
 
     @Override
     public Void call() {
-        Numbers data = generateData();
         try (Socket socket = new Socket(Constants.LOCALHOST, Constants.PORT)) {
             DataOutputStream os = new DataOutputStream(socket.getOutputStream());
-            data.writeDelimitedTo(os);
-            System.out.println("Client #" + id + " send data to server");
-
             DataInputStream is = new DataInputStream(socket.getInputStream());
-            Numbers sortedData = Numbers.parseDelimitedFrom(is);
-            checkSorting(sortedData);
+            for (int i = 0; i < x; i++) {
+                Numbers data = generateData();
+                data.writeDelimitedTo(os);
+                System.out.println("Client #" + id + " send #" + i + " data to server");
+
+                Numbers sortedData = Numbers.parseDelimitedFrom(is);
+                checkSorting(sortedData, i);
+                Thread.sleep(delta);
+            }
         } catch (IOException e) {
             System.out.println("Lost connection to server");
+        } catch (InterruptedException ignored) {
         }
         return null;
     }
 
-    private void checkSorting(Numbers numbers) {
+    private void checkSorting(Numbers numbers, int k) {
         List<Integer> list = numbers.getNumbersList();
         if (list.size() != n) {
-            System.out.println("No all data in client #" + id);
+            System.out.println("No all data #" + k + " in client #" + id);
             return;
         }
         for (int i = 0; i < n - 1; i++) {
             if (list.get(i) > list.get(i + 1)) {
-                System.out.println("Not sorted data in client #" + id);
+                System.out.println("Not sorted data #" + k + " in client #" + id);
                 return;
             }
         }
-        System.out.println("Client #" + id + " successfully received data");
+        System.out.println("Client #" + id + " successfully received #" + k + " data");
     }
 }
--- src/main/java/ru/hse/java/ClientsMain.java
+++ src/main/java/ru/hse/java/ClientsMain.java
@@ -10,12 +10,14 @@ import java.util.stream.IntStream;
 
 public class ClientsMain {
     private static final int m = 20; // number of clients
-    private static final int n = 200000; // size of array
+    private static final int n = 20; // size of array
+    private static final int x = 2;
+    private static final int delta = 200;
 
     public static void main(String[] args) throws InterruptedException, ExecutionException {
         ExecutorService clientsThreadPool = Executors.newCachedThreadPool();
         List<Future<Void>> futures = clientsThreadPool.invokeAll(
-                IntStream.range(0, m).mapToObj(i -> new Client(i + 1, n)).collect(Collectors.toList())
+                IntStream.range(0, m).mapToObj(i -> new Client(i + 1, n, x, delta)).collect(Collectors.toList())
         );
         for (Future<Void> future : futures) {
             future.get();
